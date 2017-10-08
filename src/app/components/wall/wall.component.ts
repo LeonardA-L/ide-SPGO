0086fbc5ce31a1905bde7e83348f359a16410f7f
LeonardA-L <leonard@allain-launay.fr>
2017-10-08 10:09:22 -0400

    Position elements on canvas

--- src/app/components/wall/wall.component.ts
+++ src/app/components/wall/wall.component.ts
@@ -6,6 +6,7 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { AppState } from '../../app.service';
 
@@ -22,12 +23,28 @@ import { Broadcaster } from '../../services/broadcast.service';
   styleUrls: ['wall.scss'],
 })
 export class WallComponent {
-  public elements = [
+  public elements: any[] = [
+    {
+      img: '/assets/img/elements/apple.png',
+      x: 100,
+      y: 100
+    },
+    {
+      img: '/assets/img/elements/apple.png',
+      x: 200,
+      y: 200
+    }
   ];
 
   constructor(
     public appState: AppState,
     private broadcaster: Broadcaster,
-  ) {}
+    private _sanitizer: DomSanitizer,
+  ) {
+    for (let e of this.elements) {
+      e.left = _sanitizer.bypassSecurityTrustStyle(e.x / 900 * 100 + '%');
+      e.top = _sanitizer.bypassSecurityTrustStyle(e.y / 600 * 100 + '%');
+    }
+  }
 
 }
