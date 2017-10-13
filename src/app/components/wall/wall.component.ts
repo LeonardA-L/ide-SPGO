b4c5dda75457a74e8f204afa20d91b92ff6461bb
LeonardA-L <leonard@allain-launay.fr>
2017-10-13 15:00:58 -0400

    Integrate all elements on the wall and resize images accordingly

--- src/app/components/wall/wall.component.ts
+++ src/app/components/wall/wall.component.ts
@@ -28,8 +28,25 @@ import { Element } from '../../model/element.model';
 })
 export class WallComponent {
   public elements: Element[] = [
-    new Element(0, '/assets/img/elements/1_poster_chiromancie.png', 400, 100),
-    new Element(1, '/assets/img/elements/2_peinture_foret.png', 100, 300),
+    new Element(0, '/assets/img/elements/1_poster_chiromancie.png', 81.8, 92.2, 62.8),
+    new Element(1, '/assets/img/elements/2_peinture_foret.png', 3, 42.2, 57.5),
+    new Element(2, '/assets/img/elements/3_chaines.png', 23.2, 40.1, 75.2),
+    new Element(3, '/assets/img/elements/4_fil_rouge.png', 9.3, 93.2, 11.7),
+    new Element(4, '/assets/img/elements/5_freud.png', 11.3, 31.4, 32.5),
+    new Element(5, '/assets/img/elements/6_amandier.png', 29.4, 79.78, -13.5),
+    new Element(6, '/assets/img/elements/7_coffre.png', 16.1, 21.2, 40.3),
+    new Element(7, '/assets/img/elements/8_centaure.png', 39.8, 70.0, 53.8),
+    new Element(8, '/assets/img/elements/9_la_disparue.png', 32.6, 44, 46.8),
+    new Element(9, '/assets/img/elements/10_photo_houdini.png', 19.1, 50, 8.8),
+    new Element(10, '/assets/img/elements/11_trophee_de_chasse.png', 24.8, 43.6, 42.8),
+    new Element(11, '/assets/img/elements/12_charentaises.png', 83.7, 97.7, 76.2),
+    new Element(12, '/assets/img/elements/13_roue_astrologique.png', 39.7, 52, 32.5),
+    new Element(13, '/assets/img/elements/14_assiette_coquelicot.png', 1.6, 21.8, 0.7),
+    new Element(14, '/assets/img/elements/15_harakiri.png', 69.6, 87.2, 29.4),
+    new Element(15, '/assets/img/elements/16_peinture_puits.png', 78.6, 97.8, 4.3),
+    new Element(16, '/assets/img/elements/17_escarpins.png', 85.6, 95.4, 29.5),
+    new Element(17, '/assets/img/elements/18_croquis_manoir.png', 57, 76, 4.6),
+    new Element(18, '/assets/img/elements/19_peinture_lac.png', 57, 84.6, 40.5)
   ];
 
   @Output()
@@ -45,8 +62,8 @@ export class WallComponent {
   ) {
     for (let e of this.elements) {
       e.img = _sanitizer.bypassSecurityTrustResourceUrl(e.img);
-      e.left = _sanitizer.bypassSecurityTrustStyle(e.x / 900 * 100 + '%');
-      e.top = _sanitizer.bypassSecurityTrustStyle(e.y / 600 * 100 + '%');
+      e.left = _sanitizer.bypassSecurityTrustStyle(e.x + '%');
+      e.top = _sanitizer.bypassSecurityTrustStyle(e.y + '%');
     }
   }
 
--- src/app/model/element.model.ts
+++ src/app/model/element.model.ts
@@ -2,16 +2,18 @@ export class Element {
   public img;
   public x;
   public y;
+  public width;
   public top;
   public left;
   public selected;
   public id;
 
-  constructor(_id, _img, _x, _y) {
+  constructor(_id, _img, _x, _x2, _y) {
     this.id = _id;
     this.img = _img;
     this.x = _x;
     this.y = _y;
+    this.width = (_x2 - _x) + '%';
     this.selected = false;
   }
 
