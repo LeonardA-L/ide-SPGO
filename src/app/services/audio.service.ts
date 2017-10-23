575adca51cfd6d2eeed06e3219594a3af5abf53b
LeonardA-L <leonard@allain-launay.fr>
2017-10-23 17:21:37 -0400

    Add ambient sound

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -9,20 +9,75 @@ import { Broadcaster } from './broadcast.service';
 @Injectable()
 export class AudioService {
   public audios;
+  public lastPlay = new Date();
+  private inited = false;
+  private playing = false;
+  private playloop = false;
 
-  public init() {
-    this.audios = [];
-    this.addAudio(CONFIG.root + '/assets/sounds/SPGO - 15_10_2017 02.16.wav');
+  constructor(
+    private broadcaster: Broadcaster,
+  ) {
+    const service = this;
+    this.broadcaster.on<any>('noOverlay').subscribe((event) => service.play());
   }
 
-  private addAudio(_src) {
+  public init() {
+    if (this.inited) {
+      return;
+    }
+    this.audios = [];
+    this.addAudio(CONFIG.root + '/assets/sounds/ex1.mp3', 1);
+    this.addAudio(CONFIG.root + '/assets/sounds/ex2.mp3', 1);
+    this.addAudio(CONFIG.root + '/assets/sounds/ex3.mp3', 1);
+    this.addAudio(CONFIG.root + '/assets/sounds/ex4.mp3', 0.7);
+    this.addAudio(CONFIG.root + '/assets/sounds/ex5.mp3', 0.7);
+    this.inited = true;
+    const service = this;
+  }
+
+  public play() {
+    if (this.playloop) {
+      return;
+    }
+    console.log('audioPlay');
+    this.playloop = true;
+
+    const service = this;
+    function time() {
+      setTimeout(function() {
+        service.update();
+        time();
+      }, 2000);
+    }
+    time();
+  }
+
+  private update() {
+    if (this.playloop && !this.playing && new Date().getTime() - this.lastPlay.getTime() > 10000) {
+      let audio = this.audios[Math.floor(Math.random() * this.audios.length)];
+      if (audio) {
+        this.playing = true;
+        audio.dom.currentTime = 0;
+        audio.dom.play();
+
+      }
+    }
+  }
+
+  private addAudio(_src, _master) {
     let audio = {
       src: _src,
       dom: document.createElement('audio') as any,
+      master: _master,
     };
     audio.dom.src = _src;
-    audio.dom.volume = 0;
-
+    audio.dom.volume = CONFIG.audio.master * _master;
+    const service = this;
+    audio.dom.onended = function() {
+      service.lastPlay = new Date();
+      service.playing = false;
+    };
+    /*
     audio.dom.ontimeupdate = function() {
       if (this.currentTime >= this.duration - CONFIG.audio.ramp) {
         this.volume = (this.duration - this.currentTime) / CONFIG.audio.ramp;
@@ -33,6 +88,7 @@ export class AudioService {
       }
       this.volume *= CONFIG.audio.master;
     };
+    */
 
     this.audios.push(audio);
   }
